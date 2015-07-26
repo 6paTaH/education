ce3609771a45bf3de20569dc5131319048d6d77d
JonKofee <jonkofee@live.ru>
2015-07-26 11:11:47 +0300

    Удалил всякий мусор

--- app/Controllers/StockController.php
+++ app/Controllers/StockController.php
@@ -1,12 +1,9 @@
 <?php
-
 class StockController extends Core_Controller_Abstract
 {
 
     public function manageAction()
     {
-    	$this->view->setRenderLevel(Phalcon\Mvc\View::LEVEL_ACTION_VIEW);
-    	$this->view->cars = Cars::find();
     }
 
 }
